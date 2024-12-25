050ea06d5a29ab50a3d1379b8036dd85596fa75e
Jacob Julius <jeccopatrick@outlook.com>
2024-12-25 19:32:02 +0300

    kit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,42 @@
-import { Elysia } from "elysia";
+import { PrismaClient } from "@prisma/client";
+import { Elysia,t } from "elysia";
+const prisma = new PrismaClient()
+const app = new Elysia()
+.decorate('db',prisma)
+.get("/", () => "Hello Elysia")
+.get("/manchester",async()=>{
+  return "Hello World"
+})
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+.get("/", async () => {
+  return "Unatisha"
+})
+
+.get("/products", async ({db}) => {
+  const products = await db.products.findMany({})
+
+  return products
+} )
+
+.post("/yo",async({body,db})=>{
+  const {name}=body
+  const products = await db.products.create({
+    data: {name:name}
+  })
+
+  return products
+},{
+  body:t.Object({
+    name:t.String(),
+  })
+})
+
+
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+
+
